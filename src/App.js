6fff773199e9dfd2363ead7a37657b9be113b016
Anh Kiet <Kb25092001@gmail.com>
2024-01-01 22:27:29 +0700

    update 12

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import RegisterPage from './Components/RegisterAccount';
-import React from 'react';
-import ThreeScene from './Components/ThreeScene';
 
 function App() {
   return (
@@ -11,12 +9,6 @@ function App() {
         <h1 header className="app-title"> HỒ SƠ NGƯỜI DÙNG </h1>
       </header>
       <RegisterPage />
-      <div className="three-d-component-wrapper">
-        {/* Set max-width and max-height for the container */}
-        <div style={{ maxWidth: '60px', maxHeight: '40px', overflow: 'hidden' }}>
-          <ThreeScene />
-        </div>
-      </div>
     </div>
   );
 }
